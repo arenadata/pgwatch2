18a96a68679c9f09ce8529eb4c630e95c6bd4998
Denys2002Holub <paloma20022001@gmail.com>
2022-08-04 15:21:14 +0200

    created universal simple and multiline TextField

---
+++ web-gui/src/layout/Dashboard/DbsTable/TextFieldComponents.tsx
@@ -0,0 +1,48 @@
+import { Box, TextField } from "@mui/material"
+import { useEffect, useState } from "react"
+
+type Params = {
+    id: string,
+    param: string | undefined,
+    placeholder?: string,
+    defaultValue?: string,
+    type: 'text' | 'number' | 'password'
+}
+
+export const SimpleTextField = ({ id, param, placeholder, defaultValue, type }: Params) => {
+    const [value, setValue] = useState(defaultValue)
+
+    useEffect(() => {
+        if (param === undefined && defaultValue) {
+            return
+        }
+        setValue(param)
+    }, [])
+
+    return (
+        <Box>
+            <TextField onChange={(e) => setValue(e.target.value)} value={value} type={type}
+                placeholder={placeholder} id={id} variant="standard"
+            />
+        </Box>
+    )
+}
+
+export const MultilineTextField = ({ id, param, placeholder, defaultValue, type }: Params) => {
+    const [value, setValue] = useState(defaultValue)
+
+    useEffect(() => {
+        if (param === undefined && defaultValue) {
+            return
+        }
+        setValue(param)
+    }, [])
+
+    return (
+        <Box>
+            <TextField onChange={(e) => setValue(e.target.value)} value={value} type={type}
+                placeholder={placeholder} id={id} variant="standard" multiline
+            />
+        </Box>
+    )
+}
