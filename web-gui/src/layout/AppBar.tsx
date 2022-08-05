bc53ae3a9bbd25397e9ce615713de8a01310a10a
Svitlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2022-08-05 16:20:17 +0200

    fix: AppBar links

--- web-gui/src/layout/AppBar.tsx
+++ web-gui/src/layout/AppBar.tsx
@@ -8,23 +8,25 @@ import { Link } from "react-router-dom";
 import { routes } from "./Routes";
 
 export const AppBar = () => {
+  const menuLinks = routes.map((item) => (
+    <Button
+      key={item.link}
+      to={item.link}
+      component={Link}
+      sx={{ color: "#fff" }}
+    >
+      {item.title}
+    </Button>
+  ));
+  
   return (
     <MuiAppBar component="header">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           pgwatch2
         </Typography>
-        <Box sx={{ display: { xs: "none", sm: "block" } }}>
-          {routes.map((item) => (
-            <Button
-              key={item.link}
-              to={item.link}
-              component={Link}
-              sx={{ color: "#fff" }}
-            >
-              {item.title}
-            </Button>
-          ))}
+        <Box>
+          {menuLinks}
         </Box>
       </Toolbar>
     </MuiAppBar>
