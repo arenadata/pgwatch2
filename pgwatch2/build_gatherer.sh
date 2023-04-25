f0a502175da9f354d1c69f74ca5ccc2942de3963
Sofia Rodina <rodinasophie@gmail.com>
2023-04-25 09:41:46 +0300

    add debug

--- pgwatch2/build_gatherer.sh
+++ pgwatch2/build_gatherer.sh
@@ -12,12 +12,12 @@ echo "running 'go build pgwatch2.go prom.go patroni.go logparse.go' ..."
 
 if [ -n "$GIT_TIME" -a -n "$GIT_HASH" ] ; then
   echo "running (1): go build -ldflags \"-X main.commit=$GIT_HASH -X main.date=$GIT_TIME\" pgwatch2.go prom.go patroni.go logparse.go"
-  go build -ldflags "-X main.commit=$GIT_HASH -X main.date=$GIT_TIME" pgwatch2.go prom.go patroni.go logparse.go
+  go build -gcflags "all=-N -l" -ldflags "-X main.commit=$GIT_HASH -X main.date=$GIT_TIME" pgwatch2.go prom.go patroni.go logparse.go
 
 elif [ -f build_git_version.txt ] ; then
     # Dockerfile build fills the file with HEAD hash
     echo "running (2): go build -ldflags \"-X main.commit=`cat build_git_version.txt`\" pgwatch2.go prom.go patroni.go logparse.go"
-    go build -ldflags "-X main.commit=`cat build_git_version.txt`" pgwatch2.go prom.go patroni.go logparse.go
+    go build -gcflags "all=-N -l" -ldflags "-X main.commit=`cat build_git_version.txt`" pgwatch2.go prom.go patroni.go logparse.go
 else
 
   git_on_path=$(which git)
@@ -26,10 +26,10 @@ else
     COMMIT=`git show -s --format=%H HEAD`
     DATE=`git show -s --format=%cI HEAD`
     echo "running (3): go build -ldflags \"-X main.commit=$COMMIT -X main.date=$DATE\" pgwatch2.go prom.go patroni.go logparse.go"
-    go build -ldflags "-X main.commit=$COMMIT -X main.date=$DATE" pgwatch2.go prom.go patroni.go logparse.go
+    go build -gcflags "all=-N -l" -ldflags "-X main.commit=$COMMIT -X main.date=$DATE" pgwatch2.go prom.go patroni.go logparse.go
   else
     echo "running (4): go build pgwatch2.go prom.go patroni.go logparse.go" # no  version info
-    go build pgwatch2.go prom.go patroni.go logparse.go
+    go build -gcflags "all=-N -l" pgwatch2.go prom.go patroni.go logparse.go
   fi
 
 fi
